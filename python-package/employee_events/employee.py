a4205b175403b6427dee53380d6f11c8a7f45385
TiaZhang92 <tianyizhang0092@gmail.com>
2025-05-14 14:21:39 +0000

    employee.py update

--- python-package/employee_events/employee.py
+++ python-package/employee_events/employee.py
@@ -1,25 +1,26 @@
 # Import the QueryBase class
 #### YOUR CODE HERE
-
+from query_base import QueryBase
 # Import dependencies needed for sql execution
 # from the `sql_execution` module
 #### YOUR CODE HERE
+from sql_execution import QueryMixin, query
 
 # Define a subclass of QueryBase
 # called Employee
 #### YOUR CODE HERE
-
+class Employee(QueryBase):
     # Set the class attribute `name`
     # to the string "employee"
     #### YOUR CODE HERE
-
+    name = "employee"
 
     # Define a method called `names`
     # that receives no arguments
     # This method should return a list of tuples
     # from an sql execution
     #### YOUR CODE HERE
-        
+    def names(self):    
         # Query 3
         # Write an SQL query
         # that selects two columns 
@@ -28,14 +29,19 @@
         # This query should return the data
         # for all employees in the database
         #### YOUR CODE HERE
-    
+        query_string = f"""
+            SELECT first_name || ' ' || last_name as full_name, employee_id
+            FROM {self.name}
+            ORDER BY last_name, first_name
+        """
+        return self.query(query_string)    
 
     # Define a method called `username`
     # that receives an `id` argument
     # This method should return a list of tuples
     # from an sql execution
     #### YOUR CODE HERE
-        
+    def username(self, id):    
         # Query 4
         # Write an SQL query
         # that selects an employees full name
@@ -43,7 +49,12 @@
         # to only return the full name of the employee
         # with an id equal to the id argument
         #### YOUR CODE HERE
-
+        query_string = f"""
+            SELECT first_name || ' ' || last_name as full_name
+            FROM {self.name}
+            WHERE employee_id = {id}
+        """
+        return self.query(query_string)
 
     # Below is method with an SQL query
     # This SQL query generates the data needed for
@@ -55,7 +66,7 @@
     #### YOUR CODE HERE
     def model_data(self, id):
 
-        return f"""
+        query_string = f"""
                     SELECT SUM(positive_events) positive_events
                          , SUM(negative_events) negative_events
                     FROM {self.name}
@@ -63,3 +74,4 @@
                         USING({self.name}_id)
                     WHERE {self.name}.{self.name}_id = {id}
                 """
+        return self.pandas_query(query_string)        
